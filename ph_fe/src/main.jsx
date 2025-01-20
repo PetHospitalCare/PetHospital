6ab6686d729bfc358fd50fb8b0b0eeb4dc512a79
MinhVH3 <vuhongminh07112003@gmail.com>
2025-01-20 16:25:05 +0700

    First commit

--- ph_fe/src/main.jsx
+++ ph_fe/src/main.jsx
@@ -1,11 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Button } from "@/components/ui/button"
+import App from './App.jsx'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Click me</Button> */}
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  </StrictMode>
+)
