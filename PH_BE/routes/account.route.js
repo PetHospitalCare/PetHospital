1fe94effced24a9ce8af16a3e94c79b3fc91a994
MinhVH3 <vuhongminh07112003@gmail.com>
2025-02-15 02:32:49 +0700

    Fixing CRUD account

--- PH_BE/routes/account.route.js
+++ PH_BE/routes/account.route.js
@@ -7,15 +7,8 @@ accountRouter.post("/signup", AccountController.signup);
 accountRouter.post("/signin", AccountController.signin);
 accountRouter.post("/send-otp", OTPController.sendOTP);
 accountRouter.post("/verify-otp", OTPController.verifyOTP);
-accountRouter.get("/get-all", AccountController.getAllAccount)
-accountRouter.get("/profile", verifyToken, (req, res) => {
-  res.json({ userId: req.userId, role: req.userRole });
-});
-accountRouter.get("/admin", verifyToken, authorize("admin"), (req, res) => {
-  res.json({ message: "Admin dashboard" });
-});
-accountRouter.get("/get-all", getallAccount);
-accountRouter.post("/createnewaccount", createNewAccount);
-accountRouter.delete("/delete:id", deleteAccount);
-accountRouter.put("/edit/:id", editaccount);
+accountRouter.get("/get-all", AccountController.getallAccount);
+accountRouter.post("/createnewaccount", AccountController.createNewAccount);
+accountRouter.delete("/delete:id", AccountController.deleteAccount);
+accountRouter.put("/edit/:id", AccountController.editaccount);
 module.exports = accountRouter;
