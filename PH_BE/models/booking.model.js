384fb40cfee6536e4359f12f940ef44a716faaf8
MinhVH3 <vuhongminh07112003@gmail.com>
2025-02-28 22:54:13 +0700

    Update-model-booking

--- PH_BE/models/booking.model.js
+++ PH_BE/models/booking.model.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema(
     {
-        account_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Account",
-        },
-
-
+        account_id: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
+        pet_id: { type: mongoose.Schema.Types.ObjectId, ref: "Pet" },
+        date: { type: Date },
+        hour: { type: String },
+        service_id: { type: mongoose.Schema.Types.ObjectId, ref: "Service" },
+        type: { enum: ["dog", "cat"], type: String },
+        status: { enum: ["pending", "done", "cancel"], type: String },
+        note: { type: String },
+        guest_name: { type: String, default: null },
+        guest_phone: { type: String, default: null },
+        guest_email: { type: String, default: null },
     },
     { timestamps: true }
 );
